d7a436a26ec5ff4c04c2328d5edaa51f0fc03dfd
rjowers <rjowers@g.clemson.edu>
2017-11-08 13:57:05 -0500

    created basic form and successfully gathered data from currency api

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,17 @@ var app = angular.module('myapp', []);
 
 app.controller('myController', function getLocation($scope, $rootScope, $http) {
 
+    $http({
+        method: 'GET',
+        url: 'https://api.fixer.io/latest'
+      }).then(function successCallback(response) {
+          console.log(response);
+        }, function errorCallback(response) {
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+        });
+
+
 });
 
 
