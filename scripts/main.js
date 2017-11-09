6eac70466cd2e623133dc3d7b06b6cc385769bf8
rjowers <rjowers@g.clemson.edu>
2017-11-09 14:52:14 -0500

    param from currency code now passed to get request

--- scripts/main.js
+++ scripts/main.js
@@ -4,15 +4,35 @@ var app = angular.module('myapp', []);
 
 app.controller('myController', function getLocation($scope, $rootScope, $http) {
 
-    $http({
-        method: 'GET',
-        url: 'https://api.fixer.io/latest'
-      }).then(function successCallback(response) {
-          console.log(response);
-        }, function errorCallback(response) {
-          // called asynchronously if an error occurs
-          // or server returns response with an error status.
-        });
+
+        $scope.chosenCurrency = "";
+        
+        $scope.currencies= ['AUD', 'BGN', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'EUR','GBP', 'HKD', 'HRK','HUF','IDR', 'ILS', 'INR', 'JPY', 'KRW', 'MXN', 'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RON', 'RUB', 'SEK', 'SGD', 'THB', 'TRY', 'USD', 'ZAR'];
+        
+
+          $scope.getSelectedcurrency = function() {
+            chosen = $scope.chosenCurrency;
+
+             $http({
+                 method: 'GET',
+                 url: 'https://api.fixer.io/latest?base=' + chosen,
+             }).then(function successCallback(response) {
+                console.log(response);
+             }, function errorCallback(response) {
+                ("Sorry no data :(")
+             });
+
+            };
+
+            
+        
+           
+          $scope.submitForm = function(money) {
+            $scope.data = money * 1.5094;
+
+          };
+
+
 
 
 });
